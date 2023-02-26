9124cd0558ce0dcf2cc1fb74726af0e1865b583c
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-25 20:46:07 -0600

    Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import { ErrorPage, HomePage, InvoicePage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>xD</h1>
+      {/* Place SideNavBar here */}
+
+      <Routes>
+        <Route path="/invoices/:id" element={<InvoicePage />} />
+        <Route path="/invoices" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+
+        {/* This must always be at the end */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
