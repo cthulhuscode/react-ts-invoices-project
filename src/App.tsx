95cc9c1fea5b07d6df32af397f1583c918633acb
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-28 12:50:34 -0600

    change to test pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* Place SideNavBar here */}
+      <h1>Fix pipeline</h1>
       <HashRouter>
         <Routes>
           <Route path="/invoices/:id" element={<InvoicePage />} />
