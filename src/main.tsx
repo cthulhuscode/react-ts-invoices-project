8e4126b8e4c470fe7b675461ae47e3ac8f75780d
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-02 19:41:21 -0600

    changed store by setupStore()

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { setupStore } from "./store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={setupStore()}>
       <App />
     </Provider>
   </React.StrictMode>
