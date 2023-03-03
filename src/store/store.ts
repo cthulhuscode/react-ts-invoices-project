b47a40bab726713dd7574597a3c6f66efc6efd5a
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-02 19:36:20 -0600

    adding renderWithProviders for testing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
 import { themeSlice } from "./slices";
 
-export const store = configureStore({
-  reducer: {
-    theme: themeSlice.reducer,
-  },
+// Create the root reducer independently to obtain the RootState type
+const rootReducer = combineReducers({
+  theme: themeSlice.reducer,
 });
 
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+export type AppStore = ReturnType<typeof setupStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = AppStore["dispatch"];
