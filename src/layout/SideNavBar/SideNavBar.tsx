65ba12b92acecebef3d7f2c48c56d01245d29f30
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-02 18:47:11 -0600

    added button to handle theme change

--- src/layout/SideNavBar/SideNavBar.tsx
+++ src/layout/SideNavBar/SideNavBar.tsx
@@ -1,7 +1,15 @@
 import "./SideNavBar.scss";
 import { images } from "../../constants/images";
+import { useAppDispatch } from "../../hooks/redux";
+import { toggleTheme } from "../../store";
 
 export const SideNavBar = () => {
+  const dispatch = useAppDispatch();
+
+  const handleThemeChange = () => {
+    dispatch(toggleTheme());
+  };
+
   return (
     <nav className="SideNavBar">
       <div className="SideNavBar__logo">
@@ -10,7 +18,13 @@ export const SideNavBar = () => {
       </div>
 
       <div className="SideNavBar__content">
-        <img className="SideNavBar__btnTheme" src={images.moon} alt="themple" />
+        <button className="SideNavBar__btnTheme" onClick={handleThemeChange}>
+          <img
+            className="SideNavBar__btnThemeImg"
+            src={images.moon}
+            alt="theme"
+          />
+        </button>
         <div className="SideNavBar__contentUser">
           <img className="SideNavBar__user" src={images.usuario} alt="user" />
         </div>
