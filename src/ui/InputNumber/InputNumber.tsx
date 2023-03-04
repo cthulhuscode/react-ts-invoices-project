2cb6922595f32e2c2a93adc9eda4972d47d1a1f0
Gustavo <gusavocod@gmail.com>
2023-03-03 22:05:33 -0600

    Plantillas UI

---
+++ src/ui/InputNumber/InputNumber.tsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+import "./InputNumber.scss";
+
+interface InputNumberProps {
+  variant: string;
+  children: any;
+  classes: string;
+}
+
+export const Button = ({ variant, children, classes }: InputNumberProps) => {
+  return (
+    <motion.input
+      className={`inputN${classes} inputN__${variant}`}
+      whileTap={{ scale: 0.9 }}
+    >
+      {children}
+    </motion.input>
+  );
+};
