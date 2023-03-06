be835d77267954f4db9535350db6eca50e9f6c97
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-03-05 22:09:38 -0600

    InvoiceList and InvoiceListItem add

--- src/components/InvoicesList/InvoicesList.tsx
+++ src/components/InvoicesList/InvoicesList.tsx
@@ -1,5 +1,43 @@
 import React from "react";
+import "./InvoicesList.scss";
+import { InvoicesListItem } from "../InvoicesListItem/InvoicesListItem";
 
 export const InvoicesList = () => {
-  return <div>InvoicesList</div>;
+  const lista = [
+    {
+      id: "rt3080",
+      date: "Due 19 Aug 2021",
+      name: "Jensen Huang",
+      price: "1,800.9",
+      class: "Paid",
+    },
+    {
+      id: "rt3080",
+      date: "Due 19 Aug 2021",
+      name: "Jensen Huang",
+      price: "100.9",
+      class: "Pending",
+    },
+    {
+      id: "rt3080",
+      date: "Due 19 Aug 2021",
+      name: "Jensen Huang",
+      price: "3,900.00",
+      class: "Draft",
+    },
+  ];
+  return (
+    <div className="InvoicesList">
+      {lista.map((item) => (
+        <InvoicesListItem
+          key={item.id}
+          id={item.id}
+          date={item.date}
+          name={item.name}
+          price={item.price}
+          class={item.class}
+        />
+      ))}
+    </div>
+  );
 };
--- src/components/InvoicesListItem/InvoicesListItem.tsx
+++ src/components/InvoicesListItem/InvoicesListItem.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import "./InvoicesListItem.scss";
+import { images } from "../../constants/images";
 
-export const InvoicesListItem = () => {
-  return <div>InvoicesListItem</div>;
+interface InvoicesListItemProps {
+  id: string;
+  date: string;
+  name: string;
+  price: string;
+  class: string;
+}
+
+export const InvoicesListItem = (props: InvoicesListItemProps) => {
+  return (
+    <div className="InvoicesListItem">
+      <div className="InvoicesListItem__content">
+        <h2 className="InvoicesListItem__id">
+          <span>#</span>
+          {props.id}
+        </h2>
+        <p className="InvoicesListItem__date">{props.date}</p>
+        <p className="InvoicesListItem__name">{props.name}</p>
+      </div>
+      <div className="InvoicesListItem__content">
+        <h3 className="InvoicesListItem__price">&#163; {props.price}</h3>
+        <button
+          className={`InvoicesListItem__button InvoicesListItem__button--${props.class}`}
+        >
+          {props.class}
+        </button>
+        <img
+          className="InvoicesListItem__arrow"
+          src={images.flecha}
+          alt="arrow"
+        />
+      </div>
+    </div>
+  );
 };
