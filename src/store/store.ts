2e4ef4597ffda6ec341e727fcd38818a578edf31
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-07 13:21:34 -0600

    configuring redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,41 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import type { PreloadedState } from "@reduxjs/toolkit";
-import { themeSlice } from "./slices";
+import { PERSIST, persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { themeSlice, invoicesSlice } from "./slices";
 
 // Create the root reducer independently to obtain the RootState type
 const rootReducer = combineReducers({
   theme: themeSlice.reducer,
+  invoices: invoicesSlice.reducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export function setupStore(preloadedState?: PreloadedState<RootState>) {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     preloadedState,
   });
 }
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type AppStore = ReturnType<typeof setupStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
