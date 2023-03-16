fbb2d6221b231b652534e332b39d519e5c1a7caa
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-15 18:37:00 -0600

    working on the logic

--- src/components/InvoiceForm/InvoiceForm.tsx
+++ src/components/InvoiceForm/InvoiceForm.tsx
@@ -4,9 +4,14 @@ import { motion } from "framer-motion";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { useOnClickOutside } from "../../hooks/useOnClickOutside";
 import { toggleForm } from "../../store";
-import { InputText } from "../../ui";
-import "./InvoiceForm.scss";
+
+// import type { Invoice } from "../../interfaces";
+import { Statuses } from "../../interfaces";
+
 import { ItemList } from "./ItemList/ItemList";
+import { InputText } from "../../ui";
+
+import "./InvoiceForm.scss";
 
 export const InvoiceForm = () => {
   const [fromStreet, setFromStreet] = useState("");
@@ -25,10 +30,41 @@ export const InvoiceForm = () => {
   const [toPostCode, setToPostCode] = useState("");
   const [toCountry, setToCountry] = useState("");
 
+  const [formHasErrors, setFormHasErrors] = useState(false);
+
   const ref = useRef(null);
   const dispatch = useAppDispatch();
-
   const showForm = useAppSelector((state) => state.invoices.showForm);
+
+  const handleSaveSendClick = () => {
+    if (!formHasErrors) {
+      const newInvoice = {
+        status: Statuses.pending,
+        billFrom: {
+          street: fromStreet,
+          city: fromCity,
+          country: fromCountry,
+          postCode: fromPostCode,
+        },
+        billTo: {
+          city: toCity,
+          country: toCountry,
+          postCode: toPostCode,
+          street: toStreet,
+          client: {
+            name: clientName,
+            email: clientEmail,
+          },
+        },
+        date,
+        paymentTerms,
+        projectDescription,
+      };
+
+      console.log(newInvoice);
+    }
+    alert("The form has errors");
+  };
 
   useOnClickOutside(ref, () => {
     dispatch(toggleForm(false));
@@ -42,38 +78,38 @@ export const InvoiceForm = () => {
         {/* Bill from */}
         <h4 className="iform__subtitle">Bill From</h4>
         <InputText
+          setInputHasError={setFormHasErrors}
           classes=""
           label="Street Address"
           name="fromStreet"
-          error={false}
           setState={setFromStreet}
           value={fromStreet}
         />
 
         <div className="iform__address-row iform__address-row--padding">
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="City"
             name="fromCity"
-            error={false}
             setState={setFromCity}
             value={fromCity}
           />
 
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Post Code"
             name="fromPostCode"
-            error={false}
             setState={setFromPostCode}
             value={fromPostCode}
           />
 
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Country"
             name="fromCountry"
-            error={false}
             setState={setFromCountry}
             value={fromCountry}
           />
@@ -82,56 +118,56 @@ export const InvoiceForm = () => {
         {/* Bill to */}
         <h4 className="iform__subtitle">Bill To</h4>
         <InputText
+          setInputHasError={setFormHasErrors}
           classes=""
           label="Client's Name"
           name="clientName"
-          error={false}
           setState={setClientName}
           value={clientName}
         />
 
         <InputText
+          setInputHasError={setFormHasErrors}
           classes="mt-24"
           label="Client's Email"
           name="clientEmail"
-          error={false}
           setState={setClientEmail}
           value={clientEmail}
         />
 
         <InputText
+          setInputHasError={setFormHasErrors}
           classes="mt-24"
           label="Street Address"
           name="toStreet"
-          error={false}
           setState={setToStreet}
           value={toStreet}
         />
 
         <div className="iform__address-row iform__address-row--padding">
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="City"
             name="toCity"
-            error={false}
             setState={setToCity}
             value={toCity}
           />
 
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Post Code"
             name="toPostCode"
-            error={false}
             setState={setToPostCode}
             value={toPostCode}
           />
 
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Country"
             name="toCountry"
-            error={false}
             setState={setToCountry}
             value={toCountry}
           />
@@ -139,29 +175,29 @@ export const InvoiceForm = () => {
 
         <div className="iform__address-row mt-48">
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Invoice Date"
             name="date"
-            error={false}
             setState={setDate}
             value={date}
           />
 
           <InputText
+            setInputHasError={setFormHasErrors}
             classes=""
             label="Payment Terms"
             name="paymentTerms"
-            error={false}
             setState={setPaymentTerms}
             value={paymentTerms}
           />
         </div>
 
         <InputText
+          setInputHasError={setFormHasErrors}
           classes="mt-24"
           label="Project Description"
           name="projectDescription"
-          error={false}
           setState={setProjectDescription}
           value={projectDescription}
         />
@@ -186,6 +222,7 @@ export const InvoiceForm = () => {
             <motion.button
               className="iform-btn iform-btn__save"
               whileTap={{ scale: 0.95 }}
+              onClick={handleSaveSendClick}
             >
               Save & Send
             </motion.button>
--- src/components/InvoiceForm/ItemList/ListItem/ListItem.tsx
+++ src/components/InvoiceForm/ItemList/ListItem/ListItem.tsx
@@ -1,18 +1,27 @@
 import { motion } from "framer-motion";
 
 import { images } from "../../../../constants";
+import type { InvoiceListItem } from "../../../../interfaces";
 import "./ListItem.scss";
 
-export const ListItem = () => {
+interface ListItemProps {
+  item: InvoiceListItem;
+}
+
+export const ListItem = ({ item }: ListItemProps) => {
+  const { name, amount, price, total } = item;
+
   return (
     <div className="fl-item">
       <input
+        value={name}
         className="fl-item__cell fl-item__input"
         type="text"
         name="itemName"
         id="itemName"
       />
       <input
+        value={amount}
         className="fl-item__cell fl-item__input fl-item__input-qty"
         type="number"
         name="itemQty"
@@ -21,6 +30,7 @@ export const ListItem = () => {
         placeholder="1"
       />
       <input
+        value={price}
         className="fl-item__cell fl-item__input fl-item__input-price"
         type="number"
         name="itemPrice"
@@ -28,7 +38,7 @@ export const ListItem = () => {
         placeholder="0.00"
         min={1}
       />
-      <span className="fl-item__cell fl-item__total">$156.00</span>
+      <span className="fl-item__cell fl-item__total">${total}</span>
 
       <div className="fl-item__cell">
         <motion.img
